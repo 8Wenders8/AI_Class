0ac5bd1b7f8c84a5a85138fac23460d5d4f9b959
8Wenders8 <matej.volansky15@gmail.com>
2021-10-23 23:49:43 +0200

    Started division to classes, made heuristic 1

--- Zadanie2/problem2d.py
+++ Zadanie2/problem2d.py
@@ -1,28 +1,121 @@
 import random
 
-def deckGenerator(x,y):
-    choices = random.sample(list(range(0,x*y)), k=x*y)
-    return [choices[i:i+y] for i in range(0,x*y,y)]
+class Node:
+    def __init__(self, deck, move="", parent=None):
+        self.state = deck
+        if parent == None:
+            self.depth = 0
+            self.moves = move
+        else:
+            self.depth = parent.depth
+            self.moves = parent.moves + move
 
-def heuristicsNo1(start, end,x,y):
-    print("Start:", start, "\nEnd:",end)
-    totalSum = 0
-    xyStart = {}
-    xyEnd = {}
-    # For every number from range(0,x*y) get posx posy, and diff them
-    for i,e in enumerate(start):
-        for j,ee in enumerate(e): 
-            if not ee == 0: xyStart[ee] = [i,j]
-    for i,e in enumerate(end):
-        for j,ee in enumerate(e):
-            if not ee == 0: xyEnd[ee] = [i,j]
+class Puzzle:
+    def __init__(self, x , y):
+        self.deck = self.deckGenerator(x,y)
+        self.goal = self.deckGenerator(x,y)
+        self.x = x
+        self.y = y
+        self.moves = {"U":self.up, "D":self.down, "L":self.left, "R":self.right}
 
-    xyStart = dict(sorted(xyStart.items()))
-    xyEnd = dict(sorted(xyEnd.items()))
-    print("xyStart:", xyStart, "\nxyEnd:", xyEnd)
-    for i in range(1, x*y):
-        totalSum += abs(xyStart[i][0] - xyEnd[i][0]) + abs(xyStart[i][1] - xyEnd[i][1])
+    def deckGenerator(self, x,y):
+        choices = random.sample(list(range(0,x*y)), k=x*y) # Randomly aranged list [1,7,3,8,4,5]
+        return [choices[i:i+y] for i in range(0,x*y,y)] # Make sub lists [[1,7,3],[8,4,5]]
 
-    print("Heuristika 2:",totalSum)
+    def isFinished(self):
+        return heuristicsNo1(self.deck,self.goal) == 0
 
-heuristicsNo1(deckGenerator(3,3),deckGenerator(3,3),3,3)
+    def getGapPos(self):
+        for ix, row in enumerate(self.deck):
+            for iy, value in enumerate(row):
+                if value == 0: return [ix,iy]
+
+
+    def swap(self, x1, y1, x2, y2):
+        temp = self.deck[x1][y1]
+        self.deck[x1][y1] = self.deck[x2][y2]
+        self.deck[x2][y2] = temp
+
+    def up(self):
+        gapPos = self.getGapPos()
+        if not gapPos[0] == 0: self.swap(gapPos[0], gapPos[1], gapPos[0] -1 , gapPos[1])
+
+    def down(self):
+        gapPos = self.getGapPos()
+        if not gasPos[0] == self.x - 1: self.swap(gapPos[0], gapPos[1], gapPos[0] + 1, gapPos[1])
+
+    def left(self):
+        gapPos = self.getGapPos()
+        if not gapPos[1] == 0 : self.swap(gapPos[0], gapPos[1], gapPos[0], gapPos[1] - 1 )
+
+    def right(self):
+        gapPos = self.getGapPos()
+        if not gapPos[1] == self.y - 1: self.swap(gapPos[0], gapPos[1], gapPos[0], gapPos[1] + 1)
+
+    def move(self, move):
+        if move is self.moves: self.moves[move]()
+
+        
+    def puzzlePrint(self):
+        print("Start:\t\t\tEnd:")
+        for i,row in enumerate(self.deck): 
+            print(row, "\t\t", self.goal[i])
+
+
+def isSolvable(flatStart, flatEnd, y):
+    sumStart, sumEnd  = 0, 0 
+    for index, value in enumerate(flatStart):
+        if value == 0:
+            sumStart += (index // y) + 1
+            continue
+        for j in range(index - 1, -1, -1):
+            if flatStart[j] > value and not flatStart[j] == 0: sumStart += 1
+    for index, value in enumerate(flatEnd):
+        if value == 0:
+            sumEnd += (index // y) + 1
+            continue
+        for j in range(index -1, -1, -1):
+            if flatEnd[j] > value and not flatEnd[j] == 0: sumEnd += 1
+    print(sumStart, sumEnd)
+    return sumStart % 2 == sumEnd % 2
+
+def heuristicsNo1(start, end):
+    sumSame = 0
+    for ix, row in enumerate(start):
+        for iy, value in enumerate(row):
+            if not value == end[ix][iy]: sumSame += 1
+    return sumSame
+
+def heuristicsNo2(start, end,x,y):
+    xyStart, xyEnd, totalSum = {}, {}, 0
+    for ix,row in enumerate(start):
+        for iy,value in enumerate(row): 
+            if not value == 0: xyStart[value] = [ix,iy]
+    for ix,row in enumerate(end):
+        for iy,value in enumerate(row):
+            if not value == 0: xyEnd[value] = [ix,iy]
+
+    xyStart, xyEnd = dict(sorted(xyStart.items())),  dict(sorted(xyEnd.items()))
+    #print("xyStart:", xyStart, "\nxyEnd:", xyEnd)
+    for i in range(1, x*y): totalSum += abs(xyStart[i][0] - xyEnd[i][0]) + abs(xyStart[i][1] - xyEnd[i][1])
+    return totalSum
+
+        
+def greedySolve(start, end):
+    pass
+
+x = 3
+y = 3
+#flatStart, flatEnd = [item for sublist in start for item in sublist], [item for sublist in end for item in sublist]
+p = Puzzle(3,3)
+p.puzzlePrint()
+p.up()
+p.puzzlePrint()
+p.right()
+p.up()
+p.puzzlePrint()
+
+#puzzlePrint(start, end)
+#print(isSolvable(flatStart, flatEnd, y))
+#print(heuristicsNo1(start, end))
+#print(heuristicsNo2(start, end , x, y))
