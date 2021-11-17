eefe21f6945c47e5a31fde7c21945126a0f31733
8Wenders8 <matej.volansky15@gmail.com>
2021-11-17 13:27:41 +0100

    Calculate path

--- Zadanie3/main.py
+++ Zadanie3/main.py
@@ -7,4 +7,23 @@ def create_towns(map_x, map_y, count):
     return {i: [rand.randint(0, map_x - 1), rand.randint(0, map_y - 1)] for i in range(count)}
 
 
-print(create_towns(200, 200, 20))
+def swap(a, b):
+    pass
+
+
+def shuffle_towns(map, swaps, iterate):
+    pass
+
+
+def calculate_path(point_a, point_b):
+    x = math.pow(abs(point_a[0] - point_b[0]), 2)
+    y = math.pow(abs(point_a[1] - point_b[1]), 2)
+    return math.sqrt(x + y)
+
+
+def calculate_total_cost(map):
+    pass
+
+
+# print(create_towns(200, 200, 20))
+print(calculate_path([0, 0], [1, 1]))
