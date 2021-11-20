8fbc379ca02c6dd1a77ab528180faec9490088b8
8Wenders8 <matej.volansky15@gmail.com>
2021-11-20 17:14:05 +0100

    Simulated done

--- Zadanie3/main.py
+++ Zadanie3/main.py
@@ -33,44 +33,44 @@ def shuffle_towns(town_map):
     return swap([row[:] for row in town_map], point_a, point_b)  # Copy town map by string slicing and pass it to swap
 
 
-def calculate_path(point_a, point_b):
+def distance_two_points(point_a, point_b):
     x = math.pow(abs(point_a[0] - point_b[0]), 2)  # Get the power of 2 of x difference
     y = math.pow(abs(point_a[1] - point_b[1]), 2)  # Get the power of 2 of y difference
     return math.sqrt(x + y)  # Distance between two points by pythagorean theorem
 
 
-def calculate_total_cost(town_map):
+def distance_path(town_map):
     total_cost = 0
     for i in range(len(town_map) - 1):
-        total_cost += calculate_path(town_map[i], town_map[i + 1])
+        total_cost += distance_two_points(town_map[i], town_map[i + 1])
     return total_cost
 
 
+def neighbours(current_map, perm_count):
+    permutations = queue.PriorityQueue()  # Path with lowest cost is always on the top
+    for i in range(0, perm_count):
+        shuffled = shuffle_towns(current_map)  # Randomly change two towns in path
+        permutations.put(((distance_path(shuffled)), shuffled))  # Add it to the queue, with it's cost
+    return permutations
+
+
 def hill_climbing(town_map, perm_count):
-    current = [calculate_total_cost(town_map), town_map]
+    current = [distance_path(town_map), town_map]
     while True:
-        permutations = queue.PriorityQueue()
-        for i in range(0, perm_count):
-            shuffled = shuffle_towns(current[1])
-            permutations.put(((calculate_total_cost(shuffled)), shuffled))
-        next_best = permutations.get()
+        next_best = neighbours(current[1], perm_count).get()
         if next_best[0] < current[0]:  # If a better path was found, continue generating with it
             current = [next_best[0], next_best[1]]
         else:  # If not, return the current best path ( local maximum )
             return current
 
 
-def tabu_search(town_map, perm_count, tabu_limit, iterations=1000, time_limit=15):
+def tabu_search(town_map, perm_count, tabu_limit, iterations=100, time_limit=15):
     counter, timeout = 0, time.time() + time_limit  # When to end limitation counters
     tabu_list = queue.Queue(tabu_limit)  # FIFO Queue represented as a Tabu list
-    best, current = [calculate_total_cost(town_map), town_map], [calculate_total_cost(town_map), town_map]
+    best, current = [distance_path(town_map), town_map], [distance_path(town_map), town_map]
     while time.time() < timeout and counter < iterations:
-        permutations = queue.PriorityQueue()  # Path with lowest cost is always on the top
-        for i in range(0, perm_count):
-            shuffled = shuffle_towns(current[1])  # Randomly change two towns in path
-            permutations.put(((calculate_total_cost(shuffled)), shuffled))  # Add it to the queue, with it's cost
-        counter += perm_count
-        next_best = permutations.get()  # Best path from generated pool
+        counter += 1
+        next_best = neighbours(current[1], perm_count).get()  # Best path from generated pool
         if next_best[0] < best[0]:
             best = [next_best[0], next_best[1]]
         if current[0] < next_best[0] or (current[0], current[1]) in list(tabu_list.queue):
@@ -81,8 +81,25 @@ def tabu_search(town_map, perm_count, tabu_limit, iterations=1000, time_limit=15
     return best
 
 
-def simulated_annealing(town_map):
-    pass
+def is_accepted(cost_diff, temp):
+    return rand.uniform(0, 1) < math.exp((0 - abs(cost_diff)) / temp)
+
+
+def simulated_annealing(town_map, perm_count, initial_temp, alpha):
+    temp = initial_temp
+    best, current = [distance_path(town_map), town_map], [distance_path(town_map), town_map]
+    while temp > 0.0:
+        choices = neighbours(current[1], perm_count)
+        next_best = choices.get()
+        if next_best[0] < best[0]:
+            best = [next_best[0], next_best[1]]
+        while next_best[0] >= current[0] and rand.uniform(0, 1) > (temp/100):
+            if choices.empty():
+                return best
+            next_best = choices.get()
+        current = [next_best[0], next_best[1]]
+        temp -= alpha
+    return best
 
 
 def sequence(town_map):
@@ -99,8 +116,14 @@ def print_map(town_map):
 
 t_map = create_map(200, 200, 20)
 print_map(t_map)
-print("\nStart sequence:", sequence(t_map), "\nStart cost:", "{:.2f}".format(calculate_total_cost(t_map)))
+print("\nStart sequence:", sequence(t_map), "\nStart cost:", "{:.2f}".format(distance_path(t_map)))
 hill_result = hill_climbing(t_map, 20)
 print("\nHill sequence:", sequence(hill_result[1]), "\nHill cost:", "{:.2f}".format(hill_result[0]))
-tabu_result = tabu_search(t_map, 20, 15, 10000, 20)
-print("\nTabu sequence:", sequence(tabu_result[1]), "\nTabu cost:", "{:.2f}".format(tabu_result[0]))
+start = time.time()
+tabu_result = tabu_search(t_map, 20, 50, 500, 20)
+print("\nEnd tabu time:", time.time() - start)
+print("Tabu sequence:", sequence(tabu_result[1]), "\nTabu cost:", "{:.2f}".format(tabu_result[0]))
+start = time.time()
+sa_result = simulated_annealing(t_map, 20, 100, 0.1)
+print("\nEnd SA time:", time.time() - start)
+print("SA sequence:", sequence(sa_result[1]), "\nSA cost:", "{:.2f}".format(sa_result[0]))
