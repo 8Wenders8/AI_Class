02eb8a0e30bdc1a00330f97398c3616a8089a425
8Wenders8 <matej.volansky15@gmail.com>
2021-12-05 12:27:40 +0100

    Generator done

--- Zadanie4/main.py
+++ Zadanie4/main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+import random as rand
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def generate_map(boundaries, count=20000):
+    return add_points([[rand.randint(boundaries[0], boundaries[1] - 1), rand.randint(boundaries[2], boundaries[3] - 1)] for i in range(20)], boundaries, count)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def add_points(k_means_map, boundaries, count):
+    for i in range(count):
+        interval = 100;
+        rand_index = rand.randint(0, len(k_means_map) - 1)
+        rand_point = k_means_map[rand_index]
+        rand_offset = rand.randint(0 - interval, interval)
+        while boundaries[0] < rand_point[0] + rand_offset > boundaries[1]:
+            interval -= 10
+            rand_offset = rand.randint(0 - interval, interval)
+        new_x = rand_offset
+        interval = 100
+        rand_offset = rand.randint(0 - interval, interval)
+        while boundaries[2] < rand_point[1] + rand_offset > boundaries[3]:
+            interval -= 10
+            rand_offset = rand.randint(0 - interval, interval)
+        k_means_map.append([new_x, rand_offset])
+    return k_means_map
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print(generate_map([-200, 200, -200, 200], 20))
